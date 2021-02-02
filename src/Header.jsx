ffc155beeb9a7e38279fa2ef9d8502d300db13fd
aymen <a.zitouni@esi-sba.dz>
2021-02-02 13:36:51 +0100

    Navbar

---
+++ src/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Header.css";
+import {
+  SupervisorAccount as SupervisorAccountIcon,
+  Search as SearchIcon,
+  Home as HomeIcon,
+  Chat as ChatIcon,
+  Notifications as NotificationsIcon,
+  BusinessCenter as BuisnessCenterIcon,
+} from "@material-ui/icons";
+import HeaderOption from "./HeaderOption";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://www.flaticon.com/svg/vstatic/svg/174/174857.svg?token=exp=1612259722~hmac=65cbb567addc1143983d1b14b4055b12"
+          alt=""
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BuisnessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messages" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
