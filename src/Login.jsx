46366036c5cf8eb4d1b356510a2aacadfe7be40a
aymen <a.zitouni@esi-sba.dz>
2021-02-05 17:00:35 +0100

    loginPage

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 function Login() {
+  const [name, setName] = useState("");
+  const [photoUrl, setPhotoUrl] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const loginToApp = () => {};
+  const register = () => {};
   return (
     <div className="login">
       <img
@@ -9,8 +15,37 @@ function Login() {
         alt=""
       />
       <form>
-        <input type="text" placeholder="Full name " />
+        <input
+          type="text"
+          placeholder="Full name "
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Profile pic URL"
+          value={photoUrl}
+          onChange={(e) => setPhotoUrl(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" onClick={loginToApp}>
+          Sign-in
+        </button>
       </form>
+      <p>
+        Not a member ? <span className="login__register">Register now</span>
+      </p>
     </div>
   );
 }
